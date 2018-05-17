8368388ead1f175ffc5ec50223b241c4dab633ae
Jan Brehmer <jan.brehmer@dai-labor.de>
2018-05-17 16:48:31 +0200

    added launch script performing some source and export commands

---
+++ launch_sitl_odom.sh
@@ -0,0 +1,5 @@
+. ~/efffeu_integration/devel/setup.bash
+source Tools/setup_gazebo.bash $(pwd) $(pwd)/build/posix_sitl_default
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Tools/sitl_gazebo
+roslaunch px4 mavros_posix_sitl_odom.launch
